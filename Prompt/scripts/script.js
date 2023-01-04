66c5d2aeed5166a9d58252f250e1823cc75561a3
Robson Valleriote Nascimento <93871640+RobsonVall@users.noreply.github.com>
2023-01-04 09:21:05 -0300

    Janela Prompt
    Interagindo com o usuário com a janela prompt - window.prompt(" pergunta ", " ? ") => ? argumento opcional que seria um texto padrão.

---
+++ Prompt/scripts/script.js
@@ -0,0 +1,8 @@
+var canal = "Janela - Prompt";
+var curso = "window.prompt()";
+
+const SEPARADOR = " - ";
+const QUEBRA = "\n\n"
+
+var nome = window.prompt("Digite o seu nome?",canal);
+window.alert(nome);
