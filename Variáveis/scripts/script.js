a8789563e9453fe0328326890533397522e6d743
Robson Valleriote Nascimento <93871640+RobsonVall@users.noreply.github.com>
2023-01-04 09:22:13 -0300

    Variáveis
    Declarando e manipulando variáveis - var

---
+++ Variáveis/scripts/script.js
@@ -0,0 +1,16 @@
+//var canal, nome, site; // Declaração múltiplas
+
+var canal = "Variáveis";
+var ano = 2023;
+var num = 10;
+
+curso = "Javascript"; //Declaração implicita
+curso = "Programação";
+
+var texto = canal + "<br>" + (num + ano);
+
+document.write(texto)
+
+//document.write(canal + "<br>" + num + "<br>" + curso);
+
+
