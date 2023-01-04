780c42808f94a6aea90a2bfedda1a6e9870b24ba
Robson Valleriote Nascimento <93871640+RobsonVall@users.noreply.github.com>
2023-01-04 09:10:36 -0300

    Janela Confirm
    Solicitando confirmação com a janela Confirm - window.confirm(" Solicitação ")

---
+++ Confirm/scripts/script.js
@@ -0,0 +1,5 @@
+var nome = window.prompt("Digite so seu nome:", "Entre com seu nome aqui...")
+
+var resposta = window.confirm(nome + ", deseja continuar?");
+window.alert(resposta)
+//document.write(resposta);
