d181aa867a68e42d82652a895c2708b9eccf5974
Robson Valleriote Nascimento <93871640+RobsonVall@users.noreply.github.com>
2023-01-04 09:04:09 -0300

    Janela Alert
    Emitindo aviso a janela Alert - window.alert( " aviso ")

---
+++ Alert/scrpts/script.js
@@ -0,0 +1,8 @@
+var canal = "Janela - Alert";
+var curso = "window.alert()";
+
+const SEPARADOR = " - ";
+const QUEBRA = "\n\n"
+
+//window.alert(canal + SEPARADOR + curso);
+window.alert(canal + QUEBRA + curso);
